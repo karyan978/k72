abc2ac487da40c862edbed890d5b7864c16eb139
Aryan Kumar <karyan4537@gmail.com>
2026-01-31 12:42:29 +0530

    agence page created

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => { 
   return (
-    <div className='font-[font2] text-[7vw] text-center flex justify-center gap-10 uppercase'>
-        <Link className='text-[6.5vw] leading-[7vw] border-5 border-white rounded-full px-14 pt-3 uppercase'>projects</Link>
-        <Link className='text-[6.5vw] leading-[7vw] border-5 border-white rounded-full px-16 pt-3 uppercase'>agence</Link>
+    <div className='font-[font2] text-[7vw] text-center flex justify-center mb-5 gap-10 uppercase'>
+        <Link className='text-[6.5vw] hover:border-[#d3fd50] hover:text-[#d3fd50]  leading-[6vw] border-5 border-white rounded-full px-12 pt-3 uppercase'to='/projects'>work</Link>
+        <Link className='text-[6.5vw] hover:border-[#d3fd50] hover:text-[#d3fd50] leading-[6vw] border-5 border-white rounded-full px-14 pt-3 uppercase' to='/agence'>agence</Link>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
 const Agence = () => {
+
+  
+
   return (
-    <div>
-        <div>Agence Page</div>
+   <div>
+    <div className='section1'>
+    <div className='absolute h-[20vw] rounded-3xl w-15vw top-45 left-118 overflow-hidden'>
+      <img className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2" alt="" />
     </div>
+     <div className="relative font-[font2]">
+      <div className="mt-[55vh]">
+        <div className='text-[19vw] text-center uppercase leading-[17vw]'>SEVEN7Y <br />TWO</div>
+      </div>
+      <div className='pl-[40%] mt-20'>
+        <p className='text-6xl font-[font1]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
+    </div>
+   </div>
+    <div className='section2 h-screen'>
+
+    </div>
+   </div>
   )
 }
 
